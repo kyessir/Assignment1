cb88ba45f9a2681ec3fb74b27e69b8c7c433ce16
SianKim <siankim06@gmail.com>
2023-11-30 19:41:39 +0800

    updated responsive-ness

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,24 @@
 /* hamburger menu---------------------------------------------------------------------------------------------------------*/
 document.addEventListener('DOMContentLoaded', function () {
-    const navbarToggle = document.getElementById('hamburger');
-    const navbarLinks = document.getElementById('navbar');
+    
+  // Get references to the hamburger icon and the navbar links container
+  const navbarToggle = document.getElementById('hamburger');
+  const navbarLinks = document.getElementById('navbar');
 
-    hamburger.addEventListener('click', function () {
-        navbar.classList.toggle('active');
-    });
+  // Add a click event listener to the hamburger icon
+  hamburger.addEventListener('click', function () {
+      // Toggle the 'active' class on the navbar links container
+      navbar.classList.toggle('active');
+  });
 });
 
 /*image slider---------------------------------------------------------------------------------------------------------------------*/
 $(function () {
     // Text-slider
+
+    // Handling click events for the right arrow in both text and image sliders
     $(".master-div .arrow-right").on("click", function () 
-       {
+       {// Check if the selected element is the last child
         if ($(".thumbnails .selected").is(":last-child")) {
           $(".thumbnails .selected").hide();
           $(".thumbnails p").eq(0).fadeIn(10).addClass("selected").siblings().removeClass("selected");
@@ -21,9 +27,10 @@ $(function () {
           $(".thumbnails .selected").next().fadeIn(10).addClass("selected").siblings().removeClass("selected");
         }
       });
-  
+      
+    // Handling click events for the left arrow in both text and image sliders
     $(".master-div .arrow-left").on("click", function () 
-      {
+      {// Check if the selected element is the last child
         if ($(".thumbnails .selected").is(":first-child")) {
           $(".thumbnails .selected").hide();
           $(".thumbnails p:last").fadeIn(10).addClass("selected").siblings().removeClass("selected");
@@ -34,8 +41,10 @@ $(function () {
       }
     });
     // Image Slider
-      $(".master-div .arrow-right").on("click", function () 
-       {
+    
+    // Handling click events for the right arrow in both text and image sliders
+    $(".master-div .arrow-right").on("click", function () 
+       {// Check if the selected element is the last child
         if ($(".thumbnails-img .selected-img").is(":last-child")) {
           $(".thumbnails-img .selected-img").hide();
           $(".thumbnails-img img").eq(0).fadeIn(10).addClass("selected-img").siblings().removeClass("selected-img");
@@ -44,9 +53,10 @@ $(function () {
           $(".thumbnails-img .selected-img").next().fadeIn(10).addClass("selected-img").siblings().removeClass("selected-img");
         }
       });
-  
+    
+    // Handling click events for the left arrow in both text and image sliders
     $(".master-div .arrow-left").on("click", function () 
-      {
+      {// Check if the selected element is the last child
         if ($(".thumbnails-img .selected-img").is(":first-child")) {
            $(".thumbnails-img .selected-img").hide();
            $(".thumbnails-img img:last").fadeIn(10).addClass("selected-img").siblings().removeClass("selected-img");
