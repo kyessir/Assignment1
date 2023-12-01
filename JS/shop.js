91b26c77f58d95af7225394fb9f7fdf2438926f2
SianKim <siankim06@gmail.com>
2023-12-01 22:46:30 +0800

    addition of checkout.html

--- JS/shop.js
+++ JS/shop.js
@@ -1,6 +1,6 @@
 let iconCart = document.querySelector('.iconCart');
 let cart = document.querySelector('.cart');
-let products = document.querySelector('.products');
+let productsShowcase = document.querySelector('.products');
 let close = document.querySelector('.close');
 
 iconCart.addEventListener('click', ()=>{
@@ -8,11 +8,38 @@ iconCart.addEventListener('click', ()=>{
         cart.style.right = '0';
     } else {
         cart.style.right = '-100%';
-        products.style.transform = 'translateX(0)';
+        productsShowcase.style.transform = 'translateX(0)';
     }
 })
 
 close.addEventListener('click', ()=>{
     cart.style.right = '-100%';
-    products.style.transform = 'translateX(0)';
+    productsShowcase.style.transform = 'translateX(0)';
 })
+
+let products = null;
+//get data from json file
+fetch('products.json')
+.then(response => response.json())
+.then(data => {
+    products = data;
+    addDataToHTML();
+})
+
+//show data in list
+function addDataToHTML(){
+    //remove data default to HTML
+    let wrapperHTML = document.querySelector('.products-slider');
+    wrapperHTML.innerHTML = '';
+
+    //add new data
+    if(products != null){
+        products.forEach(product =>{
+            let newProduct = document.createElement('div');
+            newProduct.classList.add('wrapper');
+            newProduct.innerHTML = 
+            '<img src="${product.image}"> <h3>${product.name}</h3> <div class = "price">${$product.price}</div> <button onclick="addCart(${product.id})>add to cart</button>';
+            listProductHTML.appendChild(newProduct);
+        })
+    }
+}
